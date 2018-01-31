4e2851e7192561d0ef566cc8d4df19a62036cba5
Junsuk Oh <a811219@gmail.com>
2018-01-31 16:26:27 +0900

    ToolbarExam 코드 수정

--- ToolbarExam/src/main/java/com/example/toolbarexam/MainActivity.java
+++ ToolbarExam/src/main/java/com/example/toolbarexam/MainActivity.java
@@ -20,22 +20,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        SearchView searchView = (SearchView) findViewById(R.id.search_view);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                // 서치뷰의 내용으로 검색을 수행할 때 호출 됨
-                Log.d(TAG, "onQueryTextSubmit: " + query);
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                // 서치뷰의 글자가 변경될 때마다 호출 됨
-                Log.d("MainActivity", "onQueryTextChange: " + newText);
-                return true;
-            }
-        });
+//        SearchView searchView = (SearchView) findViewById(R.id.search_view);
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                // 서치뷰의 내용으로 검색을 수행할 때 호출 됨
+//                Log.d(TAG, "onQueryTextSubmit: " + query);
+//                return true;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                // 서치뷰의 글자가 변경될 때마다 호출 됨
+//                Log.d("MainActivity", "onQueryTextChange: " + newText);
+//                return true;
+//            }
+//        });
 
         getSupportActionBar().setTitle("변경된 제목");
 
